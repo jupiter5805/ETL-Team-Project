af3ad25becc88694015ca3fddb6c5bee540db213
Muhammad Usman <muhammadusman5805@gmail.com>
2026-08-13 11:37:43 +0100

    s3 upload complete

--- src/ingestion/s3_export.py
+++ src/ingestion/s3_export.py
@@ -1,34 +1,55 @@
+# import boto3
+# from datetime import datetime, timezone
+
+# from src.ingestion.connection import get_totesys_connection
+# from src.ingestion.extract import extract_all_tables
+
+
+# BUCKET_NAME = "marvel-etl-project-ingestion"
+
+
+# def upload_currency_to_s3(connection):
+#     rows = extract_table(connection, "currency")
+
+#     json_body = rows_to_json(rows)
+
+
+# connection = get_totesys_connection()
+
+# s3 = boto3.client("s3")
+
+
+# for table_name, json_body in extract_all_tables(connection):
+
+#     timestamp = datetime.now(timezone.utc).strftime(
+#         "%Y-%m-%d_%H-%M-%S-%f"
+#     )
+
+#     object_key = f"{table_name}/{timestamp}.json"
+
+#     s3.put_object(
+#         Bucket=BUCKET_NAME,
+#         Key=object_key,
+#         Body=json_body,
+#         ContentType="application/json",
+#     )
+
+#     print(f"Uploaded: {object_key}")
+
+
+# connection.close()
 import boto3
-from datetime import datetime, timezone
-
-from src.ingestion.connection import get_totesys_connection
-from src.ingestion.extract import extract_all_tables
+from datetime import datetime
 
 
-BUCKET_NAME = "marvel-etl-project-ingestion"
+def upload_to_s3(table_name, json_body, bucket_name):
+    s3 = boto3.client("s3")
 
-
-connection = get_totesys_connection()
-
-s3 = boto3.client("s3")
-
-
-for table_name, json_body in extract_all_tables(connection):
-
-    timestamp = datetime.now(timezone.utc).strftime(
-        "%Y-%m-%d_%H-%M-%S-%f"
-    )
-
-    object_key = f"{table_name}/{timestamp}.json"
+    current_time = datetime.now().isoformat()
 
     s3.put_object(
-        Bucket=BUCKET_NAME,
-        Key=object_key,
+        Bucket=bucket_name,
+        Key=f"{table_name}/{current_time}.json",
         Body=json_body,
-        ContentType="application/json",
+        ContentType="application/json"
     )
-
-    print(f"Uploaded: {object_key}")
-
-
-connection.close()
