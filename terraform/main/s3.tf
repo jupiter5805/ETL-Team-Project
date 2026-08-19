1d9c7a8738d035296a02880bbcbb7fc2d36365d7
Serhan Tana <serhantana@outlook.com>
2026-08-19 01:49:58 +0100

    Add S3 trigger for transform Lambda

--- terraform/main/s3.tf
+++ terraform/main/s3.tf
@@ -24,3 +24,25 @@ resource "aws_s3_bucket_public_access_block" "processed" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+resource "aws_lambda_permission" "allow_s3_transform" {
+  statement_id  = "AllowExecutionFromS3"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.transform.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.ingestion.arn
+}
+
+resource "aws_s3_bucket_notification" "transform_trigger" {
+  bucket = aws_s3_bucket.ingestion.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.transform.arn
+    events              = ["s3:ObjectCreated:*"]
+    filter_prefix       = "raw/"
+  }
+
+  depends_on = [
+    aws_lambda_permission.allow_s3_transform
+  ]
+}
